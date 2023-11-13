5f3f362b0ce8b98cc9db8b144c10e147d2a4c8ef
BSCode <bscode502@gmail.com>
2023-11-13 02:04:03 -0800

    Add reset button

--- src/main.js
+++ src/main.js
@@ -7,6 +7,21 @@ window.addEventListener('load', function(){
     canvas.width = 1280;
     canvas.height = 720;
 
+    ctx.fillStyle = 'white';
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = 'white';
+    ctx.font = "bold 32px/1 'Fira Sans', sans-serif";
+    ctx.textAlign = "center";
+    ctx.textBaselign = "bottom";
+
+    const game = new Game(canvas);
+    game.init()
+
+    const resetBtn = this.document.getElementById('reset-btn');
+    resetBtn.onclick = function(){
+        game.reset();
+    }
+
     const fsContainer = this.document.getElementById('fullscreen-container');
 
     const fsBtn = this.document.getElementById('fullscreen-btn');
@@ -18,24 +33,14 @@ window.addEventListener('load', function(){
     tgBtn.onclick = function(){
         if(fsContainer.style.touchAction === 'auto'){
             fsContainer.style.touchAction = 'none';
-            tgBtn.innerHTML = '<i class="fa fa-gamepad"></i> Browser Touch Control: OFF';
+            tgBtn.innerHTML = '<i class="fa fa-gamepad"></i> Browser Control: OFF';
         }
         else{
             fsContainer.style.touchAction = 'auto';
-            tgBtn.innerHTML = '<i class="fa fa-gamepad"></i> Browser Touch Control: ON';
+            tgBtn.innerHTML = '<i class="fa fa-gamepad"></i> Browser Control: ON';
         }
     }
 
-    ctx.fillStyle = 'white';
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = 'white';
-    ctx.font = "bold 32px/1 'Fira Sans', sans-serif";
-    ctx.textAlign = "center";
-    ctx.textBaselign = "bottom";
-
-    const game = new Game(canvas);
-    game.init()
-    
     let lastTime = 0;
     function animate(timeStamp){
         let dt = timeStamp - lastTime;
