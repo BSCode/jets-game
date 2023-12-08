8431bab895f40af99c3bb668edef663c9e0c20bd
BSCode <bscode502@gmail.com>
2023-12-08 04:54:17 -0800

    Update player class to better fit ES6 class spec

--- src/player.js
+++ src/player.js
@@ -1,76 +1,84 @@
-const PLAYER_Y = 90;
+const PLAYER_IMG_ID = "player";
 const SPRITE_OFFSET_X = -30;
 const SPRITE_OFFSET_Y = -30;
 
-export default class Player {
-    constructor(game){
-        this.game = game;
+const PLAYER_Y = 90;
+const MOVE_CONSTRAINT_BUFFER = 5;
 
-        this.pos = {
-            'x': this.game.getWidth() * 0.5,
+export default class Player {
+    // game instance
+    #game;                      
+
+    // player sprite
+    static #spriteImg = document.getElementById(PLAYER_IMG_ID);
+    static #spriteWidth = this.#spriteImg.width * 0.5;      
+    static #spriteHeight = this.#spriteImg.height * 0.5;
+    #spritePos;                 // current pos of sprite offset from player:
+                                //  {x: val, y: val}
+    #aimTarget;                 // Y value to end aiming line
+
+    // player positioning
+    #pos;                       // player position {x: val, y: val} 
+    #moveConstraint = {};       // player move constraints {left: val, right: val}
+
+
+    constructor(game){
+        this.#game = game;
+
+        this.#pos = {
+            'x': this.#game.width * 0.5,
             'y': PLAYER_Y
         }
 
-        this.moveConstraint = []
-
-        this.image = document.getElementById("player");
-
-        this.spriteWidth = this.image.width * 0.5;
-        this.spriteHeight = this.image.height * 0.5;
-        this.spritePos = {
-            'x': this.pos.x - (this.spriteWidth * 0.5) + SPRITE_OFFSET_X,
-            'y': this.pos.y - (this.spriteHeight * 0.5) + SPRITE_OFFSET_Y
+        this.#spritePos = {
+            'x': this.#pos.x - (Player.#spriteWidth * 0.5) + SPRITE_OFFSET_X,
+            'y': this.#pos.y - (Player.#spriteHeight * 0.5) + SPRITE_OFFSET_Y
         }
 
-        this.aimTarget = this.game.getHeight();
+        this.aimTarget = this.#game.containerBottom;
     }
 
     // accessors
-    getPos(){ return this.pos; }
+    get pos() { return this.#pos; }
 
     // mutators
-    setPos(newPos){ this.pos = newPos; }
-    setMoveConstraint(left, right){ this.moveConstraint=[left, right]; }
-    setAimTarget(val){ this.aimTarget = val; }
+    set pos(newPos) { this.#pos = newPos; }
+
+    setMoveConstraint(objRadius) { 
+        this.#moveConstraint = {
+            left: this.#game.containerLeft + objRadius + MOVE_CONSTRAINT_BUFFER,
+            right: this.#game.containerRight - objRadius - MOVE_CONSTRAINT_BUFFER
+        };
+    }
 
     // render
-    draw(context){
-        if(!this.game.isGameOver()){
-
+    draw(context) {
+        if(!this.#game.isGameOver()){
             context.drawImage(
-                this.image,
-                this.spritePos.x, this.spritePos.y,
-                this.spriteWidth, this.spriteHeight
+                Player.#spriteImg,
+                this.#spritePos.x, this.#spritePos.y,
+                Player.#spriteWidth, Player.spriteHeight
             );
 
             context.beginPath();
-            context.moveTo(this.pos.x, this.pos.y);
-            context.lineTo(this.pos.x, this.aimTarget);
+            context.moveTo(this.#pos.x, this.#pos.y);
+            context.lineTo(this.#pos.x, this.#aimTarget);
             context.stroke()
-
-            // debug draw circle at center
-            // context.beginPath();
-            // context.arc(this.pos.x, this.pos.y, 10, 0, 2*Math.PI);
-            // context.save();
-            // context.globalAlpha = 0.5;
-            // context.fill();
-            // context.restore();
-            // context.stroke();
         }
     }
 
     // update
-    update(){
-        this.pos.x = Math.min(
-            this.moveConstraint[1],
-            Math.max(this.game.getInputPos().x, this.moveConstraint[0])
+    update() {
+        this.#pos.x = Math.min(
+            this.#moveConstraint.right,
+            Math.max(this.#game.input.x, this.#moveConstraint.left)
         );
 
-        this.spritePos.x = this.pos.x - this.spriteWidth * 0.5 + SPRITE_OFFSET_X;
+        this.#spritePos.x = this.#pos.x - Player.#spriteWidth * 0.5 + SPRITE_OFFSET_X;
     }
 
-    reset(){
-        this.pos.x = this.game.getWidth() * 0.5;
-        this.spritePos.x = this.pos.x - this.spriteWidth * 0.5 + SPRITE_OFFSET_X;
+    reset() {
+        this.#pos.x = this.game.width * 0.5;
+        this.#spritePos.x = this.pos.x - Player.#spriteWidth * 0.5 + SPRITE_OFFSET_X;
     }
 }
